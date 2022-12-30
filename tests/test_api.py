865b97f0bc6406054a38166bd0d89cced2788d04
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2022-12-30 15:49:32 -0500

    add test_db_init_command

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,14 +2,24 @@ import unittest
 
 from taskbox import create_app
 
+
 class ApiTestCase(unittest.TestCase):
     def setUp(self):
-        self.ctx = create_app.app_context()
+        self.app = create_app()
+        self.ctx = self.app.app_context()
         self.ctx.push()
-        self.client = app.test_client()
-    
+        self.client = self.app.test_client()
+        self.runner = self.app.test_cli_runner()
+
     def tearDown(self):
         self.ctx.pop()
+
+    def test_db_init_command(self):
+        result = self.runner.invoke(
+            args=["init-db"]
+        )
+        self.assertIn("Initialized", result.output)
+
 
 if __name__ == "__main__":
     unittest.main()
