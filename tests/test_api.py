1c24670539ff20e841d99ca3aefa18362b319c1d
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2022-12-31 13:17:42 -0500

    update

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,10 +6,10 @@ from taskbox import create_app
 class ApiTestCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app({"DATABASE": ":memory:"})
-        self.app.test_cli_runner().invoke(args=["init-db"])
         self.client = self.app.test_client()
         self.ctx = self.app.app_context()
         self.ctx.push()
+        self.app.test_cli_runner().invoke(args=["init-db"])
 
     def tearDown(self):
         self.ctx.pop()
