b8d3f67fda860a4ec2952b3fbe282d1997f524f3
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-01-04 07:31:53 -0500

    update

--- taskbox/run.py
+++ taskbox/run.py
@@ -32,7 +32,7 @@ def action(name: str, task_id: int):
     :type task_id: int
     
     """
-    configuration = get_db().execute("select actions from tasks where task_id = ?", (task_id,)).fetchone()
+    task = get_db().execute("select * from tasks where task_id = ?", (task_id,)).fetchone()
     if "ACTIONS" not in current_app.config:
         return "No ACTIONS provided in config.py", 400
     if name not in current_app.config["ACTIONS"]:
@@ -40,5 +40,5 @@ def action(name: str, task_id: int):
     obj = current_app.config["ACTIONS"][name]
     if not isinstance(obj({}), Action):
         return "Does not conform to Action interface", 400
-    obj_loaded = obj(loads(configuration))
+    obj_loaded = obj(loads(task["actions"]))
     return obj_loaded.jsonify()
