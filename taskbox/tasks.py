996abe37f1049d29195d745c3797a9aaec830997
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-01-06 07:47:17 -0500

    update

--- taskbox/tasks.py
+++ taskbox/tasks.py
@@ -6,7 +6,7 @@ from subprocess import PIPE
 
 from flask import Blueprint
 from flask import flash
-from flask import redict
+from flask import redirect
 from flask import request
 from flask import render_template
 from flask import url_for
