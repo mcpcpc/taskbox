c2feee88166f19289c5f2f96e2affda50b841218
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-01-06 17:55:33 -0500

    update

--- taskbox/auth.py
+++ taskbox/auth.py
@@ -91,7 +91,7 @@ def login():
             session["user_id"] = user["id"]
             return redirect(url_for("index"))
         flash(error)
-    return render_template("auth/login.html")
+    return render_template("login.html")
 
 
 @auth.route("/logout")
