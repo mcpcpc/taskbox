ec43a1f4f00cfe9a94eaea05b3d9db9d65130d0b
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-01-11 09:53:37 -0500

    Update api.py

--- taskbox/api.py
+++ taskbox/api.py
@@ -39,4 +39,5 @@ def token_required(view):
 @api.route("/task/<int:id>", methods=("GET",))
 @token_required
 def read_task(id: int):
-    return get_db().execute("SELECT * FROM task WHERE id = ?", (id,)).fetchone()
+    task = get_db().execute("SELECT * FROM task WHERE id = ?", (id,)).fetchone()
+    return dict(task)
