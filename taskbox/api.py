1bf002ea46908fea18c2e4d7a899bc587e197f89
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-01-11 09:38:09 -0500

    Update api.py

--- taskbox/api.py
+++ taskbox/api.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 from functools import wraps
 
 from flask import Blueprint
 from flask import current_app
 from flask import request
-from itsdangerous import TimedJSONSignatureSerializer
-from itsdangerous import BadSignature
-from itsdangerous import SignatureExpired
+from jwt import encode as jwt_encode
+from jwt import decode as jwt_decode
+from jwt.exceptions import InvalidSignatureError
+from jwt.exceptions import ExpiredSignatureError
 
-from db import get_db
+from taskbox.db import get_db
 
 api = Blueprint("api", __name__, url_prefix="/api")
 
@@ -19,23 +21,22 @@ def token_required(view):
     @wraps(view)
     def wrapped_view(**kwargs):
         if request.authorization is None:
-            return "Authorization is missing", 401
+            return "Authorization is missing.", 401
         elif reqest.authorization["password"] is None:
-            return "Token missing", 401
+            return "Token missing.", 401
         token = reqest.authorization["password"]
         secret_key = current_app.config["SECRET_KEY"]
-        serializer = TimedJSONSignatureSerializer(secret_key)
         try:
-            serializer.loads(token)
-        except SignatureExpired:
-            return "Token missing", 401
-        except BadSignature:
-            return "Token has bad signature", 401
+            jwt_decode(token, secret_key, algorithms=["HS256"])
+        except ExpiredSignatureError:
+            return "Token is expired.", 401
+        except InvalidSignatureError:
+            return "Token has invalid signature.", 401
         return view(**kwargs)
     return wrapped_view
 
 
-@api.route("/task/<int:id>", methods=(,"GET"))
+@api.route("/task/<int:id>", methods=("GET",))
 @token_required
 def read_task(id: int):
-    return get_db().execute("SELECT * FROM task").fetchone()
+    return get_db().execute("SELECT * FROM task WHER id = ?", (id,)).fetchone()
