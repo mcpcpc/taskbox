3f3223ce69e5e49f4216924c6cf040a2405da91b
Michael Czigler <contact@mcpcpc.com>
2023-01-12 12:28:48 +0000

    simplify token error checking

--- taskbox/api.py
+++ taskbox/api.py
@@ -7,11 +7,7 @@ from functools import wraps
 from flask import Blueprint
 from flask import current_app
 from flask import request
-from jwt import encode as jwt_encode
 from jwt import decode as jwt_decode
-from jwt.exceptions import InvalidSignatureError
-from jwt.exceptions import ExpiredSignatureError
-from jwt.exceptions import DecodeError
 
 from taskbox.db import get_db
 
@@ -23,20 +19,12 @@ def token_required(view):
 
     @wraps(view)
     def wrapped_view(**kwargs):
-        if request.authorization is None:
-            return "Authorization is missing.", 401
-        elif request.authorization["password"] is None:
-            return "Token missing.", 401
-        token = request.authorization["password"]
         secret_key = current_app.config["SECRET_KEY"]
         try:
+            token = request.authorization["password"]
             jwt_decode(token, secret_key, algorithms=["HS256"])
-        except ExpiredSignatureError:
-            return "Token is expired.", 401
-        except InvalidSignatureError:
-            return "Token has invalid signature.", 401
-        except DecodeError:
-            return "Token is malformed.", 401
+        except Error as error:
+            return error, 401
         return view(**kwargs)
 
     return wrapped_view
